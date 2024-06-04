76763e2fbe0a0d236d58a0a46cf527f0a3047440
Inshan Bhattarai <inshanbhattarai2045@gmail.com>
2024-06-04 20:55:15 +0545

    idandsecretkey hidden

--- recommender/spotify_client.py
+++ recommender/spotify_client.py
@@ -4,6 +4,6 @@ from django.conf import settings
 
 def get_spotify_client():
     auth_manager = SpotifyClientCredentials(
-        client_id='140259c128a34245b6e8be78e47aadf2',
-        client_secret='7ed5430949f24d7ca4fc340e6748630b')
+        client_id='',
+        client_secret='')
     return spotipy.Spotify(auth_manager=auth_manager)
